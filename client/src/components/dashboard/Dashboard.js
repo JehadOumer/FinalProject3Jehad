a59607e8ae1c5e4f8ae211b5d4578bf64fe92d7c
Jehad <oumerjehad@yahoo.com>
2020-09-01 12:44:30 +0200

    third commit

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profileActions";
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.getCurrentProfile();
+  }
+  render() {
+    return <div>Dashboard</div>;
+  }
+}
+export default connect(null, { getCurrentProfile })(Dashboard);
